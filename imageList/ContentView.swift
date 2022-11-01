2c0f2267e0ff1cb887ea5aeb2c8792d4869afdfc
Eduardo Thomas <eduardothomas64@gmail.com>
2022-11-01 13:54:05 -0500

    Initial Commit

---
+++ imageList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  imageList
+//
+//  Created by Backtrack on 11/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ imageList/imageListApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageListApp.swift
+//  imageList
+//
+//  Created by Backtrack on 11/01/22.
+//
+
+import SwiftUI
+
+@main
+struct imageListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
